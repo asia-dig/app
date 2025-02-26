6ea32cbf208f8d80f02e8b9a36d6e29706b9b998
asia-dig <cs_digital@icloud.com>
2025-02-27 01:11:21 +0700

    Update node.js

--- node.js
+++ node.js
@@ -13,3 +13,15 @@ nvm current # Should print "v22.14.0".
 
 # Verify npm version:
 npm -v # Should print "10.9.2".
+updater.dispatcherfrom telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Halo! Ini adalah bot saya.")
+
+updater = Updater("7973622617:AAEnVPbdrzUr1hJ6niAtXWee_cIudAd4dtA")
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
